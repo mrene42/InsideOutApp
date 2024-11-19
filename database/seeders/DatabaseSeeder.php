bd8692ae9939c53833769c603cf33227cf7390e7
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-19 11:06:46 +0100

    build: seed journal table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Journal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Journal::factory(10)->create();
     }
 }
