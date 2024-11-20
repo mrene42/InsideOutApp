5822e7a2372e14153e25898a4dbac062837a6b3d
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-20 10:04:07 +0100

    build: creating route to show function

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JournalController;
 
-Route::get('/', [JournalController::class, 'index' ]);
+Route::get('/', [JournalController::class, 'index' ])->name('home');
+Route::get('/journals/{id}', [JournalController::class, 'show'])->name('showDetail');
