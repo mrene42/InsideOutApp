581c652ac78b0392659d64bd5e979e36063eebc6
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-20 11:43:37 +0100

    build: created show function and view

---
+++ resources/views/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+    <a href="{{ route('home') }}" class="btn btn-secondary">Volver</a>
+    <div class="details">
+        <img src="" alt="{{ $journal->emotion }}">
+        <h4>{{ $journal->created_at }}</h4>
+        <p>{{ $journal->entry }}</p>
+    </div>
+@endsection
